aacf65839106f86ee58b43209d974a35d63b0445
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-03-17 19:32:51 +1100

    Completed Week2 Exercise

--- Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/example/layouts/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.example.layouts
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.layouts.ui.theme.LayoutsTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +35,80 @@ class MainActivity : ComponentActivity() {
         setContent {
             LayoutsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    SeasonScreenPreview()
                 }
             }
         }
     }
+}
+
+@Composable
+fun SeasonBtn(title: String)
+{
+    val context = LocalContext.current
+    var msg = ""
+    when (title) {
+        "Spring" -> msg = "September, October, November"
+        "Summer" -> msg = "December, January, February"
+        "Autumn" -> msg = "March April May"
+        "Winter" -> msg = "June, July, August"
+        else -> { // Note the block
+            print("None")
+        }
+    }
+    ElevatedButton(
+        onClick = {
+            Toast.makeText(context,msg,Toast.LENGTH_SHORT).show()
+        }
+    ) {
+        Text(title)
+    }
+}
+
+@Composable
+fun SeasonScreen(modifier: Modifier = Modifier)
+{
+    val imgUrl = painterResource(R.drawable.seasons)
+    Column(
+        modifier = modifier.fillMaxWidth().padding(top = 50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Surface(
+            color = MaterialTheme.colorScheme.primary,
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Text("Seasons and Months",
+                    modifier = modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+                fontSize = 24.sp)
+        }
+        Surface(
+            modifier = modifier.padding(top = 20.dp)
+        )
+        {
+            Image(
+                painter = imgUrl,
+                contentDescription = null,
+                modifier = modifier.padding(horizontal = 24.dp)
+            )
+        }
+        Row(
+            modifier = modifier.fillMaxWidth().padding(top = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            SeasonBtn("Spring")
+            SeasonBtn("Summer")
+            SeasonBtn("Autumn")
+            SeasonBtn("Winter")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SeasonScreenPreview()
+{
+    SeasonScreen()
 }
 
 @Composable
