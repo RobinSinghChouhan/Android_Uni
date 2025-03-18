1213e88e4c3c12d4da3f4570956550a1d5625b90
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-03-19 02:25:44 +1100

    Tested second approach for using .value with mutableState

--- TextFieldLab/app/src/main/java/com/example/textfieldlab/MainActivity.kt
+++ TextFieldLab/app/src/main/java/com/example/textfieldlab/MainActivity.kt
@@ -6,24 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.textfieldlab.ui.theme.TextFieldLabTheme
-import kotlin.math.abs
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,23 +35,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RememberName()
 {
-    var name by remember { mutableStateOf("") }
+//    var name by remember { mutableStateOf("") }
+    val name = remember { mutableStateOf("") }
     Column(modifier = Modifier.padding(16.dp))
     {
         Spacer(
             modifier = Modifier.height(32.dp)
         )
-        if(name.isNotEmpty())
+        if(name.value.isNotEmpty())
         {
             Text(
-                text = "Hello, $name",
+                text = "Hello, ${name.value}",
                 modifier= Modifier.padding(bottom = 8.dp),
                 fontSize = 40.sp
             )
         }
         OutlinedTextField(
-            value = name,
-            onValueChange = { name = it },
+            value = name.value,
+            onValueChange = { name.value = it },
             label = {Text("Name")}
         )
     }
