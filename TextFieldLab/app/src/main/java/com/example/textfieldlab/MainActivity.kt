3a540d58473e0f80002e083e97cf1243499dfd49
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-03-19 13:44:34 +1100

    Part 4 Date Picker Exercise

--- TextFieldLab/app/src/main/java/com/example/textfieldlab/MainActivity.kt
+++ TextFieldLab/app/src/main/java/com/example/textfieldlab/MainActivity.kt
@@ -1,45 +1,152 @@
 package com.example.textfieldlab
 
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.textfieldlab.ui.theme.TextFieldLabTheme
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.Date
+import java.util.Locale
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TextFieldLabTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    RandomNumberGenerator(modifier = Modifier.padding(innerPadding))
+//                    RandomNumberGenerator(modifier = Modifier.padding(innerPadding))
+                        DisplayDatePicker(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun DisplayDatePicker(modifier: Modifier = Modifier)
+{
+    val calendar = Calendar.getInstance()
+    var birthday by remember { mutableStateOf("") }
+    val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = Instant.now().toEpochMilli()
+    )
+
+    var showDatepicker by remember {
+        mutableStateOf(false)
+    }
+
+    var selectedDate by remember {
+        mutableLongStateOf(calendar.timeInMillis)
+    }
+
+    Column(modifier = modifier.padding(50.dp))
+    {
+        TextField(
+            value = birthday,
+            onValueChange = {},
+            readOnly = true,
+            label = {Text("Birthday")},
+            modifier = Modifier.fillMaxWidth().clickable {
+                showDatepicker = true
+            },
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.calendar),
+                    contentDescription = null,
+                    modifier = Modifier.clickable {
+                        showDatepicker = true
+                    }.size(40.dp)
+                )
+            }
+        )
+        if(showDatepicker)
+        {
+            DatePickerDialog(
+                onDismissRequest = {
+                    showDatepicker = false
+                },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            showDatepicker = false
+                            selectedDate = datePickerState.selectedDateMillis!!
+                            birthday = "DoB: ${formatter.format(Date(selectedDate))}"
+                        }
+                    ) {
+                        Text("OK")
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            showDatepicker = false
+                        }
+                    ) {
+                        Text("Cancel")
+                    }
+                }
+            ) {
+                DatePicker(
+                    state = datePickerState
+                )
+            }
+        }
+    }
+
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true)
+@Composable
+fun DisplayDatePickerPreview()
+{
+    DisplayDatePicker()
 }
 
 @Composable
