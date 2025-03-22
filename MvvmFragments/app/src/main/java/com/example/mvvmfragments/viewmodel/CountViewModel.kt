2f009b64970efe253964be6122aed0ca54925d6f
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-03-23 01:32:35 +1100

    Implemented CountViewModel in Fragments

---
+++ MvvmFragments/app/src/main/java/com/example/mvvmfragments/viewmodel/CountViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.mvvmfragments.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CountViewModel : ViewModel(){
+
+    private var count = MutableLiveData<Int>()
+    init {
+        count.value = 0
+    }
+
+    fun getCountLiveData() : LiveData<Int> {
+        return count
+    }
+
+    fun increment() {
+        count.value = count.value!!+1
+    }
+
+    fun decrement() {
+        count.value = count.value!! - 1
+    }
+}
