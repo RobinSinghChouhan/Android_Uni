bdbbbb830043308d3eef619d069ad43de201de85
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-04-09 01:48:28 +1000

    Added basic About Screen

--- BottomNavigationBar/app/src/main/java/com/example/bottomnavigationbar/About.kt
+++ BottomNavigationBar/app/src/main/java/com/example/bottomnavigationbar/About.kt
@@ -1,4 +1,36 @@
 package com.example.bottomnavigationbar
 
-class About {
+import android.graphics.Paint.Align
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun About() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "About the App",
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+            Text("This app was created as part of FIT5046 exercise",
+                style = MaterialTheme.typography.bodyLarge)
+        }
+    }
 }
