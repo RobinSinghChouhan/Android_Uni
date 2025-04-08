8a84ecb8b8780fa6d35f1ca10d606cfdde78c38a
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-04-09 02:01:51 +1000

    Added Bottom Navigation Bar.

--- BottomNavigationBar/app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
+++ BottomNavigationBar/app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.bottomnavigationbar.ui.theme.BottomNavigationBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +32,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BottomNavigationBarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android ",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            BottomNavigationBar()
+//            BottomNavigationBarTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android ",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
         }
     }
 }
@@ -43,5 +58,57 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     BottomNavigationBarTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun BottomNavigationBar() {
+    val navRoutes = listOf(
+        NavRoute("home", Icons.Filled.Home,"Home"),
+        NavRoute("profile",Icons.Filled.Person,"Profile"),
+        NavRoute("about",Icons.Filled.Info,"About")
+    )
+
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigation(
+                modifier = Modifier.padding(bottom = 20.dp),
+                backgroundColor = Color(0xffadd8e6)
+            ) {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+
+                navRoutes.forEach{ navRoute ->
+                    BottomNavigationItem(
+                        icon = { Icon(navRoute.icon, contentDescription = navRoute.label) },
+                        label = {Text(navRoute.label)},
+                        selected = currentDestination?.route == navRoute.route,
+                        onClick = {
+                            navController.navigate(navRoute.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    inclusive = false
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) {
+        paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = "home",
+            modifier = Modifier.padding(paddingValues)
+        )
+        {
+            composable("home") { Home() }
+            composable("profile") {  Profile() }
+            composable("about") { About() }
+        }
     }
 }
---
+++ BottomNavigationBar/app/src/main/java/com/example/bottomnavigationbar/NavRoute.kt
@@ -0,0 +1,6 @@
+package com.example.bottomnavigationbar
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavRoute(val route: String, val icon: ImageVector, val label: String) {
+}
