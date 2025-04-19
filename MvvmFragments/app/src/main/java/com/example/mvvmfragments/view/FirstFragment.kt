80e7348c52b3a27ef8787e0a1fb5b5b965f2b022
Robin Singh Chouhan <robinsingh.02@gmail.com>
2025-04-20 02:20:40 +1000

    Added strings.xml

--- MvvmFragments/app/src/main/java/com/example/mvvmfragments/view/FirstFragment.kt
+++ MvvmFragments/app/src/main/java/com/example/mvvmfragments/view/FirstFragment.kt
@@ -1,17 +1,16 @@
 package com.example.mvvmfragments.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.mvvmfragments.R
 import com.example.mvvmfragments.databinding.FragmentFirstBinding
 import com.example.mvvmfragments.viewmodel.CountViewModel
-import kotlin.math.absoluteValue
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -35,6 +34,7 @@ class FirstFragment : Fragment() {
 
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
